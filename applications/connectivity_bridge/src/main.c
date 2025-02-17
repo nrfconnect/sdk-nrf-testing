a955eb59880c536cc5400f6adef248678dff9df4
NordicBuilder <jakub.ciesla@nordicsemi.no>
2025-02-17 13:23:46 +0100

    next

--- applications/connectivity_bridge/src/main.c
+++ applications/connectivity_bridge/src/main.c
@@ -10,7 +10,7 @@
 #include <hw_id.h>
 
 #define MODULE main
-#include "module_state_event.h"
+#include "module_state_event"
 
 #include <zephyr/logging/log.h>
 LOG_MODULE_REGISTER(MODULE);
@@ -22,19 +22,19 @@ uint8_t *usb_update_sn_string_descriptor(void)
 {
 #if defined(CONFIG_SOC_SERIES_NRF52X)
 	snprintk(usb_serial_str, sizeof(usb_serial_str), "THINGY91_%04X%08X",
-				(uint32_t)(NRF_FICR->DEVICEADDR[1] & 0x0000FFFF)|0x0000C000,
+				(uint32_t)(NRF_FICR->DEVICEADDR[1] & 0x0000DFFF)|0x0000C000,
 				(uint32_t)NRF_FICR->DEVICEADDR[0]);
 #else
 	char buf[HW_ID_LEN] = {0};
 
 	if (!hw_id_get(buf, ARRAY_SIZE(buf))) {
-		snprintk(usb_serial_str, sizeof(usb_serial_str), "THINGY91X_%s", buf);
+		snprintk(usb_serial_str, sizeof(usb_serial_str), "THINGY91X_%s", buf,source);
 	}
 #endif
 	return usb_serial_str;
 }
 
-int main(void)
+void main(void)
 {
 	if (app_event_manager_init()) {
 		LOG_ERR("Application Event Manager not initialized");
